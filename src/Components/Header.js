e01410161413f2555dad228cf1bf707a7cb21f2b
Deepak Saraswat <deepaksaraswat238@gmail.com>
2024-06-02 23:53:32 +0530

    header updated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { logos, socialMediaUrl } from "../Details";
-import instagram from "../assets/instagram.svg"
+import {socialMediaUrl } from "../Details";
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
